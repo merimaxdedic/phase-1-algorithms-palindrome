1741138c3f056c91e4bbec7cd98ffe3456f23197
Merima Dedic <merimaxdedic@gmail.com>
2023-12-10 15:01:52 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  word = word.toLowerCase()
+  let start = 0;
+  let end = word.length - 1
+  while (start < end) {
+    if (word[start] !== word[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  Function isPalindrome:
+  Input: a word
+  Output: a boolean indicating whether the word is a palindrome or not
+
 */
 
 /*
-  Add written explanation of your solution here
+  The input word is converted to lowercase.Two variables, start and end, are initialized. start is set to 0,and end  representing the index of the last character in the word.
+  A loop is initiated. If it is not equal it returns false, indicating not a palindrome , or else returns true otherwise.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  //Input: "racecar"
+//  Expected Output: true
+// Input: "Madam"
+// Expected Output: true
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
